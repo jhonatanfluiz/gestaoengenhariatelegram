98474f82558a2a6390e134532450f9ac9682809d
Jhonatan Luiz <jhonatanfluiz@gmail.com>
2026-05-29 09:44:48 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gestaoengenhariatelegram/sw.js', { scope: '/gestaoengenhariatelegram/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
